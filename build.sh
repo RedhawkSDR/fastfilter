a62f9eb9c04fb077de2dbcc6458f28fc00700206
Ryan Bauman <ryan.bauman@axiosengineering.com>
2014-08-20 11:19:04 -0400

    Post release versioning update

--- build.sh
+++ build.sh
@@ -23,9 +23,9 @@ if [ "$1" = "rpm" ]; then
     if [ -e fastfilter.spec ]; then
         mydir=`dirname $0`
         tmpdir=`mktemp -d`
-        cp -r ${mydir} ${tmpdir}/fastfilter-1.0.0
-        tar czf ${tmpdir}/fastfilter-1.0.0.tar.gz --exclude=".svn" -C ${tmpdir} fastfilter-1.0.0
-        rpmbuild -ta ${tmpdir}/fastfilter-1.0.0.tar.gz
+        cp -r ${mydir} ${tmpdir}/fastfilter-1.0.1
+        tar czf ${tmpdir}/fastfilter-1.0.1.tar.gz --exclude=".svn" -C ${tmpdir} fastfilter-1.0.1
+        rpmbuild -ta ${tmpdir}/fastfilter-1.0.1.tar.gz
         rm -rf $tmpdir
     else
         echo "Missing RPM spec file in" `pwd`
